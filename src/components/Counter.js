14c4452e9366b1cadddd114a2c4ebcaf79978ddb
Leah Cardoz <leahcardoz@gmail.com>
2023-10-05 11:51:34 -0400

    added some bootstrap classes to elements

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,27 @@
 import { React, useState } from 'react';
 
-const Counter = () => {
+const Counter = () => { 
 
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState({
+    count: 0,
+    imageUrl: 'https://picsum.photos/200'
+  });
+
+  const formatCount = () => data.count === 0 ? 'Zero' : data.count;
+
+  const handleIncrement = () => {
+    setData({
+      ...data,
+      count: data.count + 1
+    })
+  }
 
   return (
     <>
-      <span>{count}</span>
-      <button onClick={()=> setCount(count+1)}>Increment</button>
+      <img src={data.imageUrl} alt="sunset" />
+      <br />
+      <span className="badge badge-primary m-2">{formatCount()}</span>
+      <button className="btn btn-secondary btn-sm" onClick={handleIncrement}>Increment</button>
     </>
   )
 }
